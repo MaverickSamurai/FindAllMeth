a4b83153112cb4d486180171a0af9ce53abb60a2
MaverickSamurai <mirsamirba@code.edu.az>
2022-07-17 16:24:33 +0400

    FindAllMeth

---
+++ FindAllMeth/FindAllMeth/Models/MyFindMeth.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindAllMeth.Models
+{
+    class MyFindMeth<T>
+    {
+
+        private T[] _arr;
+
+        public int Count { get; set; }
+
+        public MyFindMeth()
+        {
+            _arr = new T[10];
+        }
+
+        public void Add(T data)
+        {
+            if (Count != 0 && Count % 10 == 0)
+            {
+                Array.Resize(ref _arr, _arr.Length + 10);
+            }
+            _arr[Count] = data;
+            Count++;
+
+        }
+
+
+        public void GetAll()
+        {
+            foreach (var item in _arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public T FindAll(Predicate<T> predicate)
+        {
+            for (int i = 0; i < _arr.Length; i++)
+            {
+                if (predicate(_arr[i]))
+                {
+                    Console.WriteLine(_arr[i]);
+                }
+            }
+            return default;
+        }
+    }
+}
---
+++ FindAllMeth/FindAllMeth/Program.cs
@@ -0,0 +1,24 @@
+﻿using FindAllMeth.Models;
+using System;
+
+namespace FindAllMeth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyFindMeth<string> names = new MyFindMeth<string>();
+
+            names.Add("Mirsamir");
+            names.Add("Elmir");
+            names.Add("Semi");
+            names.Add("Niyam");
+            names.Add("Lazim");
+            names.Add("Elmir");
+
+            var result = names.FindAll(m => m == "Elmir");
+
+            Console.WriteLine(result);
+        }
+    }
+}
